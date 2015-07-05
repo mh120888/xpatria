ce1617d8844023fe1deb240188ba4efefda67b5e
Matt Higgins <matthew.alan.higgins@gmail.com>
2015-07-04 20:25:48 -0700

    Modifies index and page pages to have a bit of padding, adds contact form to contact page.

--- wp-content/themes/xpatria-theme/page.php
+++ wp-content/themes/xpatria-theme/page.php
@@ -1,14 +1,17 @@
 <?php get_header(); ?>
 
-<?php if ( have_posts() ) : while ( have_posts() ) :  the_post();  ?>
-
+<main>
+  <section>
+    <?php if ( have_posts() ) : while ( have_posts() ) :  the_post();  ?>
   <h1><?php the_title() ?></h1>
-  <p><?php the_content() ?></p>
 
-<?php endwhile; else: ?>
+    <p><?php the_content() ?></p>
+
+  <?php endwhile; else: ?>
 
   <p><?php _e('Sorry, there are no posts'); ?></p>
 
 <?php endif; ?>
-
+</section>
+</main>
 <?php get_footer(); ?>
