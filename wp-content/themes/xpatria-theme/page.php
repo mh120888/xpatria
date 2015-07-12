f234a650bc137e8f1ac35902239e504ee98c9005
Matt Higgins <matthew.alan.higgins@gmail.com>
2015-07-12 08:09:54 -0700

    Adds responsive styles

--- wp-content/themes/xpatria-theme/page.php
+++ wp-content/themes/xpatria-theme/page.php
@@ -1,9 +1,9 @@
 <?php get_header(); ?>
 
 <main>
-  <section>
+  <section class='main'>
     <?php if ( have_posts() ) : while ( have_posts() ) :  the_post();  ?>
-  <h1><?php the_title() ?></h1>
+  <!-- <h1><?#php the_title() ?></h1> -->
 
     <p><?php the_content() ?></p>
 
